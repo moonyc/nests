16191bbbbbd478f98e79dff5602ce92c4e7250bd
muni <booscarface@gmail.com>
2023-10-02 10:50:33 +0200

    get and controller

--- scratch/src/app.controller.ts
+++ scratch/src/app.controller.ts
@@ -1,11 +1,20 @@
 import { Controller, Get } from '@nestjs/common'
 
+/*
+    we use the controller decorator to control
+    some high level routing rules
+ */
 
 @Controller() // decorator
 class AppController {
     @Get()
     getRootRoute() {
         return 'hi there'
+    }
+
+    @Get('/bye')
+    getByeThere() {
+        return 'bye there'
     }
 }
 
